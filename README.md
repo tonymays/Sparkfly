926352b1e991e11c6a587e715bb9feabaf718436
Anthony Mays <anthonymays@Anthonys-MBP.home>
2021-11-21 21:50:32 -0600

    check in of test2

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Sparkfly
 
-Test 1
+##Test 1
 proc_codes.go handles the first test given
+
+```
+cd sparkfly/proc_codes
+```
 
 ```
 go run proc_codes.go
@@ -16,4 +20,28 @@ cleaning up and exiting gracefully
 No test routine was written
 
 
-Test 2
+##Test 2
+compress_file.go handles the second test given
+
+```
+cd sparkfly/compress_file
+```
+
+```
+go run compress_file.go
+```
+
+Outputs
+```
+api_user@Dev-Kernel-API:~/sparkfly/compress_file$ go run compress_file.go
+api_user@Dev-Kernel-API:~/sparkfly/compress_file$ ll
+total 108
+drwxrwxr-x 2 api_user api_user  4096 Nov 22 03:46 ./
+drwxrwxr-x 4 api_user api_user  4096 Nov 20 05:25 ../
+-rw-rw-r-- 1 api_user api_user  1951 Nov 22 03:45 compress_file.go
+-rw-rw-r-- 1 api_user api_user 86017 Nov 21 23:06 test.csv
+-rw-rw-r-- 1 api_user api_user  5480 Nov 22 03:46 test.gz
+api_user@Dev-Kernel-API:~/sparkfly/compress_file$
+```
+
+No test routine was written
--- compress_file/compress_file.go
+++ compress_file/compress_file.go
@@ -1,28 +1,103 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"compress/gzip"
-	"fmt"
+	"errors"
 	"io"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	// what file do I want to zip
+	fn := "test.csv"
+
+	// open the file
+	f, err := os.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+
+	// compress the file's contents
+	data, err := Compress(f)
+	if err != nil {
+		panic(err)
+	}
+
+	// let's get a proper extension and maybe a cup of tea
+	gz, err := ChangeExt(fn, "gz")
+	if err != nil {
+		panic(err)
+	}
+
+	// this is where you write to S3 but I am doing it locally
+	cf, _ := os.Create(gz)
+	w := gzip.NewWriter(cf)
+	w.Write(data)
+
+	// close both version of the file
+	w.Close()
+	f.Close()
 }
 
+type MyCompressor interface {
+	ChangeExt(fn string, ext string) (string, error)
+	Compress(rc io.ReadCloser) ([]byte, error)
+}
 
-func NewGzipReader(source io.ReadCloser) io.Reader {
-	r, w := io.Pipe()
-	go func() {
-		defer w.Close()
+func Compress(rc io.ReadCloser) ([]byte, error) {
+	// read the contents of the file
+	reader := bufio.NewReader(rc)
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
 
-		zip, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		defer zip.Close()
-		if err != nil {
-			w.CloseWithError(err)
-		}
+	// convert read bytes to string
+	str := string(data)
 
-		io.Copy(zip, source)
-	}()
-	return r
+	// create a byte buffer
+	var b bytes.Buffer
+
+	// zip the string to a byte buffer
+	gz := gzip.NewWriter(&b)
+	_, err = gz.Write([]byte(str))
+	if err != nil {
+		return nil, err
+	}
+
+	// close the gzip writer
+	gz.Close()
+
+	// return the compressed bytes for storage
+	return b.Bytes(), nil
+}
+
+func ChangeExt(fn string, ext string) (string, error) {
+	// throw error if missing fn is empty
+	if len(fn) == 0 {
+		return "", errors.New("filename not specified")
+	}
+	// throw error if ext is empty
+	if len(ext) == 0 {
+		return "", errors.New("ext not specified")
+	}
+
+	//  where's the dot (.)
+	idx := strings.LastIndex(fn, ".")
+
+	// dot was found, then ...
+	if idx > -1 {
+		// ... give me the filename up to but not including the dot
+		fn = fn[0:idx]
+	}
+
+	// please note: if no dot was found then you are asking this routine
+	// to suffix an extension to a file that does really have one
+
+	// suffix the extension
+	return fn + "." + ext, nil
 }
